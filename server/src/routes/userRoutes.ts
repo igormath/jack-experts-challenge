bece5b7d342d3998eaac59bf4427cf523f589632
igormath <igorxt9@gmail.com>
2024-08-31 17:53:26 -0300

    add user routes

---
+++ server/src/routes/userRoutes.ts
@@ -0,0 +1,29 @@
+import { Router, Request, Response } from "express";
+import { CreateUserController } from "../controllers/user/CreateUserController";
+import { GetUserController } from "../controllers/user/GetUserController";
+import { DeleteUserController } from "../controllers/user/DeleteUserController";
+import { PutUserController } from "../controllers/user/PutUserController";
+
+const router = Router();
+
+router.get('/user', async (req: Request, res: Response) => {
+    return new GetUserController().handle(req, res);
+})
+
+router.get('/user/:email', async (req: Request, res: Response) => {
+    return new GetUserController().handle(req, res);
+})
+
+router.post('/user', async (req: Request, res: Response) => {
+    return new CreateUserController().handle(req, res);
+})
+
+router.delete('/user/:email', async (req: Request, res: Response) => {
+    return new DeleteUserController().handle(req, res);
+})
+
+router.put('/user/:email', async (req: Request, res: Response) => {
+    return new PutUserController().handle(req, res);
+})
+
+export default router;
