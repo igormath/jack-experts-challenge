c35019e5dc59f07aa603c8d5cbe7b13030a1029b
igormath <igorxt9@gmail.com>
2024-09-05 21:45:10 -0300

    update Home receiving email via ReactRouter custom hook

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,15 +1,22 @@
+import { useLocation } from "react-router-dom";
 import Header from "../Header/Header";
 
 const Home = () => {
-  return (
+    const location = useLocation();
+    const email = location.state?.email;
+
+    return (
     <>
         <Header/>
+        <nav className="h-10 bg-slate-300">
+            <a href="/" className=" bg-red-700">Voltar para a página inicial</a>
+        </nav>
         <main className="font-roboto">
-            <h2 className="m-4  text-xl">Bem-vindo, User.Name.</h2>
+            <h2 className="m-4  text-xl">Bem-vindo, User.Name {email}</h2>
             {/* Assistir algum video de TO-DO list para pegar inspiração para essa tela */}
         </main>
     </>
-  )
+    )
 }
 
 export default Home;
