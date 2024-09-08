398705c5defee840fcd83dbe5c4f5032495a44e1
igormath <igorxt9@gmail.com>
2024-09-08 15:29:45 -0300

    add edit task render card

---
+++ client/src/components/TaskCard/TaskCard.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import ModalRemoveTask from "../ModalRemoveTask/ModalRemoveTask";
+import ModalEditTask from "../ModalEditTask/ModalEditTask";
+import { TaskProps } from "../Home/Home";
+
+interface TaskCardProps {
+    id: number,
+    done: boolean,
+    authorEmail: string,
+    children: string,
+    onTaskDeleted: () => void;
+    onTaskUpdated: (updatedTask: TaskProps) => void;
+}
+
+const TaskCard = (props: TaskCardProps) => {
+
+    const [showModalRemove, setShowModalRemove] = useState<boolean>(false);
+    const [showModalEdit, setShowModalEdit] = useState<boolean>(false);
+    
+    return (
+    <li className=" border border-slate-500 rounded-md my-4 p-2 md:w-1/2 md:mx-auto">
+        <span className="font-medium">Descrição:</span>
+        {props.done ? <p className="line-through">{props.children}</p> : <p>{props.children}</p>} 
+        {props.done ? <p className="font-medium">Tarefa completa.</p> : <p className="font-medium">Tarefa incompleta.</p>} 
+        <div className="flex row justify-end">
+            <button onClick={() => setShowModalEdit(true)} className="h-8 w-2/6 mr-2 block rounded bg-slate-800 hover:bg-slate-700 text-primary text-center md:w-1/6">Editar</button>
+            <button onClick={() => setShowModalRemove(true)} className="h-8 w-2/6 block rounded bg-red-500 hover:bg-red-400 text-primary text-center md:w-1/6">Remover</button>
+            <ModalRemoveTask
+                isVisible={showModalRemove}
+                onClose={() => setShowModalRemove(false)}
+                email={props.authorEmail}
+                id={props.id}
+                onTaskDeleted={props.onTaskDeleted}
+            />
+            <ModalEditTask 
+                isVisible={showModalEdit} 
+                onClose={() => setShowModalEdit(false)} 
+                email={props.authorEmail} 
+                id={props.id}
+                description={props.children}
+                done={props.done}
+                onTaskUpdated={props.onTaskUpdated}
+            />
+        </div>
+    </li>
+    )
+}
+
+export default TaskCard;
+
